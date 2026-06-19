5af803cb5d1fea1fea9b665cf73bc4fda01dabdb
raphaelmoreira22 <72143336+raphaelmoreira22@users.noreply.github.com>
2026-06-19 13:27:53 -0300

    Add files via upload

---
+++ supabase_client.py
@@ -0,0 +1,14 @@
+from supabase import create_client
+
+SUPABASE_URL = "pvaxxoresjkerxszsgtk.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2YXh4b3Jlc2prZXJ4c3pzZ3RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NTQxMTUsImV4cCI6MjA5NzQzMDExNX0.G4Tr-HS-brBRO7jOyXGta0xrYqXUtZvXlAmC4PtdfaA"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+def get_manobras():
+    return supabase.table("manobras").select("*").execute().data
+
+
+def add_manobra(dado):
+    return supabase.table("manobras").insert(dado).execute()
