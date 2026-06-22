986a42abb11c7e76e96cd6fa60a700a1e748e2b9
raphaelmoreira22 <72143336+raphaelmoreira22@users.noreply.github.com>
2026-06-22 08:13:59 -0300

    Add files via upload

--- Painel.py
+++ Painel.py
@@ -40,6 +40,13 @@ def normalizar_df(df: pd.DataFrame) -> pd.DataFrame:
 # =========================
 @st.cache_data(ttl=60)
 def carregar_banco():
+
+st.write("Colunas encontradas:")
+st.write(df.columns.tolist())
+
+st.write("Primeiros registros:")
+st.dataframe(df.head())
+    
     try:
         response = supabase.table("manobras").select("*").execute()
         df = pd.DataFrame(response.data)
